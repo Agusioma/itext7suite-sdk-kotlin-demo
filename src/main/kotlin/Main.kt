d938ab6ec753a0dd8c55ca29509113dbc7673450
Agusioma <terryterrence200@gmail.com>
2022-04-26 02:07:04 +0300

    fichecksum

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,41 @@
+import java.io.FileInputStream
 import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.Paths
+import java.security.MessageDigest
 
 
 fun main(args: Array<String>) {
     val fileName = "/home/incognito/Downloads/Resume.pdf"
     val path: Path = Paths.get(fileName)
     val sizeInBytes = Files.size(path)
-    println(sizeInBytes)
+
+    val md = MessageDigest.getInstance("MD5")
+
+
+    val hex = checksum(fileName, md)
+    println(hex)
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
     //println("Program arguments: ${args.joinToString()}")
+}
+
+
+private fun checksum(filepath: String, md: MessageDigest): String? {
+
+    // DigestInputStream is better, but you also can hash file like this.
+    FileInputStream(filepath).use { fis ->
+        val buffer = ByteArray(1024)
+        var nread: Int
+        while (fis.read(buffer).also { nread = it } != -1) {
+            md.update(buffer, 0, nread)
+        }
+    }
+
+    // bytes to hex
+    val result = StringBuilder()
+    for (b in md.digest()) {
+        result.append(String.format("%02x", b))
+    }
+    return result.toString()
 }
