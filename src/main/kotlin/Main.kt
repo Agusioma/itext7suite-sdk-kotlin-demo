c3248b15530e37cc5c29635a8e02fc392a7e7fd2
Agusioma <terryterrence200@gmail.com>
2022-04-26 20:17:43 +0300

    final

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,5 @@
-
 import khttp.extensions.fileLike
-import okhttp3.*
-import okhttp3.MediaType.Companion.toMediaTypeOrNull
-import okhttp3.MultipartBody.Builder
-import okhttp3.RequestBody.Companion.asRequestBody
-import okhttp3.RequestBody.Companion.toRequestBody
+import khttp.responses.Response
 import org.json.JSONArray
 import org.json.JSONObject
 import java.io.File
@@ -17,96 +12,45 @@ import java.security.MessageDigest
 
 fun main(args: Array<String>) {
     val fileName = "/home/incognito/Downloads/Resume.pdf"
+
+    //getting the size in bytes
     val path: Path = Paths.get(fileName)
     val sizeInBytes = Files.size(path)
-    val baseUrl = "http://localhost:8080/api/job"
-    // val files = listOf(File("/home/incognito/Downloads/Resume.pdf").fileLike())
-    val md = MessageDigest.getInstance("MD5")
-    val checksum = fileCheckSum(fileName, md)
 
-    val postPayload = jsonPayload(checksum, "Resume.pdf", sizeInBytes)
-    //val request = post("http://localhost:8080/api/job", files = files, data = postPayload)
-    // val e = post(baseUrl,json = postPayload, files = files)
+    //getting the file checksum
+    val msgDigest = MessageDigest.getInstance("MD5")
+    val checksum = fileCheckSum(fileName, msgDigest)
 
-
-  //  val loadee = postPayload.toString()
-    val loadee = mapOf("job" to postPayload)
-    print(loadee)
-  //  val client = HttpClient.newBuilder().build();
-  /*  val request = HttpRequest.newBuilder()
-        .uri(URI.create("http://localhost:8080/api/job"))
-        .POST(HttpRequest.BodyPublishers.ofString(loadee))
-        .header("Content-Type", "application/x-www-form-urlencoded")
-        .build()
-    val response = client.send(request, HttpResponse.BodyHandlers.ofString());*/
-  //  println(loadee)
-    val files = listOf(File("/home/incognito/Downloads/Resume.pdf").fileLike())
-
-   /* val client: OkHttpClient = OkHttpClient().newBuilder()
-        .build()
-    val mediaType = "text/plain".toMediaTypeOrNull()
-
-    val OCTET: MediaType? = "application/octet-stream".toMediaTypeOrNull()
-    var filee = File(fileName)
-   // val fileRequestBody: RequestBody = filee.toRequestBody(OCTET)
-    val fileRequestBody: RequestBody = filee.asRequestBody(OCTET)
-
-    val JSON: MediaType? = "application/json".toMediaTypeOrNull()
-    val requestBody: RequestBody = loadee.toRequestBody(JSON)
-    val body: RequestBody = Builder().setType(MultipartBody.FORM)
-        .addFormDataPart("job", null, requestBody)
-        .addFormDataPart("file","Resume.pdf", fileRequestBody)
-        .build()
-
-    var request:Request = Request.Builder()
-        .url("http://localhost:8080/api/job")
-        .method("POST", body)
-        .build();
-    val response =
-        client.newCall(request).execute()
-    println(response.code)*/
-    //request.newBuilder()
-    //val r = post(url, files = files)
-val boon = khttp.post("http://localhost:8080/api/job", files = files, data=loadee)
-    print(boon.text)
-
-   /*
-
-    val loadee2 = mapOf("job" to postPayload).toList()
-    print(loadee2)
-    val (request, response, result) = Fuel.post("http://localhost:8080/api/job")
-
-        //.jsonBody(loadee)
-        .upload().add { FileDataPart(File("/home/incognito/Downloads/Resume.pdf"), filename = "Resume.pdf") }
-        .response()
-
-    println(response)*/
+    //getting the JSON payload
+    val postPayload = jsonPayload(checksum, "<filename>", sizeInBytes)
+    val jobData = mapOf("job" to postPayload)
+    val files = listOf(File("<filepath>").fileLike())
+    val response = khttp.post("<address>/api/job", files = files, data = jobData)
+    print(response.text)
 
 }
 
-
-public fun fileCheckSum(filepath: String, md: MessageDigest): String {
+fun fileCheckSum(filepath: String, msgDigest: MessageDigest): String {
 
     FileInputStream(filepath).use { fis ->
         val buffer = ByteArray(1024)
         var nread: Int
         while (fis.read(buffer).also { nread = it } != -1) {
-            md.update(buffer, 0, nread)
+            msgDigest.update(buffer, 0, nread)
         }
     }
 
     val result = StringBuilder()
-    for (b in md.digest()) {
-        result.append(String.format("%02x", b))
+    for (byte in msgDigest.digest()) {
+        result.append(String.format("%02x", byte))
     }
     return result.toString()
 }
 
-public fun jsonPayload(checkSum: String, fileName: String, fileSize: Long): JSONObject {
+fun jsonPayload(checkSum: String, fileName: String, fileSize: Long): JSONObject {
     val reqSpec = JSONObject()
     val reqSpecItems = JSONObject()
     val srcFilesItems = JSONObject()
-    val srcFiles = JSONObject()
     val srcFilesArray = JSONArray()
 
     reqSpecItems.put("type", "PDF_TO_IMAGES")
@@ -115,7 +59,6 @@ public fun jsonPayload(checkSum: String, fileName: String, fileSize: Long): JSON
     reqSpecItems.put("defaultPageScaling", 1)
     reqSpec.put("request", reqSpecItems)
 
-    //val srcFilesItems = mapOf("checksum" to checkSum, "fileType" to "application/pdf", "fileSize" to fileSize, "fileName" to fileName)
     srcFilesItems.put("checksum", checkSum)
     srcFilesItems.put("fileType", "application/pdf")
     srcFilesItems.put("fileSize", fileSize)
@@ -126,3 +69,16 @@ public fun jsonPayload(checkSum: String, fileName: String, fileSize: Long): JSON
     return reqSpec
 
 }
+
+fun getStatus(jobID: String){
+    val response : Response = khttp.get("<address>/api/"+jobID)
+    //write your processing logic here
+    println(response)
+}
+
+fun downloadResult(jobID: String){
+    val response : Response = khttp.get("<address>/api/"+jobID+"/file/result")
+    val jpegData : ByteArray = response.content
+    //write your saving logic here
+}
+
